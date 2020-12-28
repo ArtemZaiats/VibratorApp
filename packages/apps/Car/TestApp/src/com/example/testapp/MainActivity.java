1957f1b9ad8b453db68ee8c40c6f254afbe85cff
Artem Zaiats <artem.zaiats@globallogic.com>
2020-12-28 17:36:15 +0200

    Added vibration button
    The device is vibrates for 1 second and sends message to the logs when the button is pressed
    
    Signed-off-by: Artem Zaiats <artem.zaiats@globallogic.com>

--- packages/apps/Car/TestApp/src/com/example/testapp/MainActivity.java
+++ packages/apps/Car/TestApp/src/com/example/testapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.testapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button vibrateButton = (Button) findViewById(R.id.button);
+        vibrateButton.setOnClickListener(v -> {
+            Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+            VibrationEffect effect = VibrationEffect.createOneShot(1000, VibrationEffect.DEFAULT_AMPLITUDE);
+            vibrator.vibrate(effect);
+            Log.v("vibrator", "vibration on!");
+        });
     }
+
 }
